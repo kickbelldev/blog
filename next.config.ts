19b599ced23b39fc6a7e985e2fd012a4bed534dd
Jongchan <kickbelldev@gmail.com>
2025-07-10 19:50:39 +0900

    setting: tailwind, shadcn, mdx, 폰트 설정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
-import type { NextConfig } from "next";
+import createMDX from '@next/mdx'
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  pageExtensions: [
+    'js',
+    'jsx',
+    'md',
+    'mdx',
+    'ts',
+    'tsx',
+  ],
+  output: 'export',
+}
 
-export default nextConfig;
+const withMDX = createMDX({
+  extension: /\.(md|mdx)$/,
+})
+
+export default withMDX(nextConfig)
